b902d74bd84da21bde9d8616255d6ad67eca33db
andy-keene <keene.andy.t@gmail.com>
2017-07-29 23:04:15 -0700

    add GET events route

--- server/app.js
+++ server/app.js
@@ -10,12 +10,14 @@ var db = new sqlite3.Database('events.db');
 db.run("CREATE TABLE IF NOT EXISTS events (time TEXT, remote_ip TEXT, host_ip TEXT, remote_port TEXT, service TEXT, type TEXT, host_longitude INTEGER, host_latitude INTEGER, remote_longitude INTEGER, remote_latitude INTEGER, remote_country_name TEXT, remote_city TEXT, username TEXT, password TEXT, description TEXT)");
 
 
-// server and middleware for both 
-// url-encoded and json POST decoding
+
 var server = express();
 
+// server and middleware for both url-encoded and json POST decoding
+// and serving static content
 server.use(parser.urlencoded({'extended': false}));
 server.use(parser.json());
+server.use('/', express.static(__dirname + '/static/'));
 
 //basic root
 server.get('/', function(req, res) {
@@ -29,6 +31,7 @@ server.get('/', function(req, res) {
 });
 
 //POST events for storage
+//this could use some work on error checking
 server.post('/event', function(req, res) {
 
 	res.status(200);
@@ -38,10 +41,10 @@ server.post('/event', function(req, res) {
 
 	db.serialize(function() {
 		var stmt = db.prepare(`INSERT INTO events (time, remote_ip, host_ip, remote_port, 
-						      service, type, host_longitude, host_latitude, remote_longitude, 
-						      remote_latitude, remote_country_name, remote_city, username, password, 
-						      description) 
-						      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`);
+						       service, type, host_longitude, host_latitude, remote_longitude, 
+						       remote_latitude, remote_country_name, remote_city, username, password, 
+						       description) 
+						       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`);
 		stmt.run(req.body.time, req.body.remote_ip, req.body.host_ip,
 				 req.body.remote_port, req.body.service, req.body.type,
 				 req.body.host_longitude, req.body.host_latitude, req.body.remote_longitude,
@@ -50,15 +53,37 @@ server.post('/event', function(req, res) {
 		stmt.finalize();
 	}); //end insert
 
-
-	console.log("Here is the body we recieved: ");
-	console.log(req.body);
-
-	//console.log(req.body.remote_ip);
-	res.send('Thank you');
-
+	res.send('ok');
 });
 
+//GET events from storage
+server.get('/events', function(req, res) {
+
+	res.set({
+		'Content-Type': 'application/json'
+	});
+
+	db.all('SELECT * FROM events', function(err, rows){
+		if(err){
+			res.status(500);
+			res.send({
+				"error": JSON.stringify(err),
+				"events": []
+			});
+		} 
+		else {
+			var events = [];
+			rows.forEach(function(row, index, arr){
+				events.push(JSON.stringify(row));
+			});
+			res.status(200);
+			res.send({
+				"error": null,
+				"events": events
+			});
+		}
+	});
+});
 
 //app kick off!
 server.listen(8080);
