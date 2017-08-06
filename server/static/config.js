2a36b9dcafdbecb4c06567f864e5f3607441117b
andy-keene <keene.andy.t@gmail.com>
2017-08-06 13:34:57 -0700

    update map for socket

---
+++ server/static/config.js
@@ -0,0 +1,38 @@
+var config = {
+	"dataProvider": {
+		"type": "map",
+		"theme": "light",
+
+		"dataProvider": {
+			"map": "worldLow",
+			"lines": [],
+			"images": []
+		},
+		"areasSettings": {
+			"unlistedAreasColor": "#8dd9ef"
+		},
+		"imagesSettings": {
+			"color": "#FF0000",
+			"rollOverColor": "#585869",
+			"selectedColor": "#585869",
+			//"pauseDuration": 0.2,
+			//"animationDuration": 2.5,
+			//"adjustAnimationSpeed": true
+		},
+		"linesSettings": {
+			"color": "#585869",
+			"alpha": 0.4
+		},
+		"export": {
+			"enabled": true
+		}
+	},
+	"serviceColors": {
+		"telnet": "#2E696A",
+		"ssh": "#626AD2"
+	},
+	"svgs": {
+		"plane": "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z",
+		"target": "m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47",
+	},
+}
